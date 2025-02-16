4c75cef4f9890084de0dd98e87dae4678a1e9496
ig-imanish <manishkukran123@gmail.com>
2025-02-16 19:47:01 +0530

    final commit

--- index.js
+++ index.js
@@ -36,3 +36,10 @@ const headerLogoConatiner = document.querySelector('.header__logo-container')
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
 })
+function validateFileSize(input) {
+  const maxSize = 10 * 1024 * 1024; // 10MB in bytes
+  if (input.files[0].size > maxSize) {
+    alert('File size must be less than 10MB');
+    input.value = '';
+  }
+}
